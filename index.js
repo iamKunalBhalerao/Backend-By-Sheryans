238930ad64ce0e50292025d2166ba375966dcd16
iamKunalBhalerao <kunalbhalerao789@gmail.com>
2025-03-03 17:19:48 +0530

    Connected Ejs Frontend to Expres server

--- index.js
+++ index.js
@@ -1,30 +1,12 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({
-    message: "This is intro to the express JS",
-  });
-});
-
-app.get("/about", (req, res) => {
-  res.json({
-    message: "This is about Page",
-  });
-});
-
-app.get("/profile", (req, res) => {
-  res.json({
-    message: "This is Profile Page",
-  });
-});
-
-app.post("/signin", (req, res) => {
-  res.json({
-    message: "This is POST request Page",
-  });
+  res.render("index");
 });
 
 app.listen(3000, () => {
